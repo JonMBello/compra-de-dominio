cd100d47b213111b14d0654a3d7f3f67c6b2fce5
Jonathan Martinez <jonathanmtzbello@gmail.com>
2021-07-12 17:49:27 -0500

    Modelo de Registro DNS

--- models/DnsRecord.js
+++ models/DnsRecord.js
@@ -1,2 +1,27 @@
 //TODO
 //Modelo del Registro DNS
+let {DataTypes, Deferrable} = require('sequelize');
+let {db} = require('../db');
+const Dominio = require('./Dominio');
+
+const DnsRecord = db.define('DnsRecord',{
+    Dom_ID:{
+        type : DataTypes.BIGINT,
+        references : {
+            model: Dominio,
+            key: 'Dom_ID',
+            deferrable: Deferrable.INITIALLY_IMMEDIATE
+        }
+    },
+    Dns_Name:{
+        type : DataTypes.STRING
+    },
+    Dns_Data:{
+        type : DataTypes.STRING
+    },
+    Dns_Type:{
+        type : DataTypes.STRING
+    }
+});
+
+module.exports = DnsRecord;
