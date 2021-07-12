5976d4a07707f2ca98a53c8aac41954bb3264e32
Jonathan Martinez <jonathanmtzbello@gmail.com>
2021-07-12 17:25:43 -0500

    Modelo de Instancia

--- models/Instancia.js
+++ models/Instancia.js
@@ -1,2 +1,30 @@
 //TODO
 //Modelo de la Instancia
+let {DataTypes, Deferrable} = require('sequelize');
+let {db} = require('../db');
+const Usuario = require('./Usuario');
+
+const Instancia = db.define('Instancia',{
+    Usr_ID:{
+        type : DataTypes.BIGINT,
+        references : {
+            model: Usuario,
+            key: 'Usr_ID',
+            deferrable: Deferrable.INITIALLY_IMMEDIATE
+        }
+    },
+    Ins_Name:{
+        type : DataTypes.STRING
+    },
+    Ins_Status:{
+        type : DataTypes.STRING
+    },
+    Ins_Expires:{
+        type : 'TIMESTAMP'
+    },
+    Ins_RenewDeadline:{
+        type : 'TIMESTAMP'
+    }
+});
+
+module.exports = Instancia;
