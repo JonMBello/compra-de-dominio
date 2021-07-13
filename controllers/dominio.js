04085d5812abb1b4876403fd4ff41f30e9265a5e
Jonathan Martinez <jonathanmtzbello@gmail.com>
2021-07-13 15:22:23 -0500

    Probable corte de luz

--- controllers/dominio.js
+++ controllers/dominio.js
@@ -1,3 +1,7 @@
+const axios = require('axios');
+
+const Dominio = require('../models/Dominio')
+
 let obtenerDominio = async (req, res, next) => {
     //TODO
     //Opción para obtener el ID desde el token y el dominio desde la URL
@@ -77,27 +81,124 @@ let obtenerDominios = async (req, res, next) => {
 }
 
 let registrarDominio = async (req, res, next) => {
-    let body = req.body;
     //TODO
+    //Opción para obtener el id del token
+    // let Usr_id = req.Usr_ID;
+    let Usr_ID = req.body.id;
+    let { gd, mp } = req.body;
     //Verificar datos
-    //Enviar petición a GoDaddy
-    //Evaluar respuesta
-    //Sí
-        //Guardar datos en BD
-        //Cobrar al usuario
-        //Regresar datos al usuario
-        res.status(201).json({
-            msg : `Post, crear uno`,
-            obj : body
-        });
-    //No
-        //Guardar datos en BD en caso de ser necesario
-        //Regresar datos de error al usuario
-        res.status(500).json({
-            error : {
-                msg : 'Error del sistema, intente de nuevo más tarde o comuníquese con un asesor'
+    try {
+        const response = await axios ({
+            method : 'post',
+            url : `${process.env.URL}/v1/domains/purchase/validate`,
+            data : gd,
+            headers : {
+                'Authorization' : process.env.Auth
             }
         });
+        console.log(response.status);
+        //Evaluar respuesta
+        if(response.status != 200){
+            res.status(response.status).json({
+                error : response.data
+            });
+        }
+    } catch (error) {
+        console.log(error.response.status);
+        console.log(error.response.statusText);
+        console.log(error.response.data);
+        res.status(error.response.status).json({
+            error : error.response.data
+        });
+    }
+    //TODO
+    //Cobrar al usuario 
+    let pago = req.pago;
+    if(pago){
+        //Enviar petición de compra a GoDaddy
+        try {
+            const response = await axios ({
+                method : 'post',
+                url : `${process.env.URL}/v1/domains/purchase`,
+                data : gd,
+                headers : {
+                    'Authorization' : process.env.Auth
+                }
+            });
+            console.log(response.status);
+            //Evaluar respuesta
+            if(response.status != 200){
+                res.status(response.status).json({
+                    error : response.data
+                });
+            }
+        } catch (error) {
+            console.log(error.response.status);
+            console.log(error.response.statusText);
+            console.log(error.response.data);
+            res.status(error.response.status).json({
+                error : error.response.data
+            });
+        }
+        //TODO  
+        //Buscar datos del dominio
+        let domGD;
+        try {
+            domGD = await axios ({
+                method : 'get',
+                url : `${process.env.URL}/v1/domains/${gd.domain}`,
+                headers : {
+                    'Authorization' : process.env.Auth
+                }
+            });
+            console.log(domGD.status);
+            console.log(domGD.data);
+            //Evaluar respuesta
+            if(domGD.status != 200){
+                res.status(domGD.status).json({
+                    error : domGD.data
+                });
+            }
+        } catch (error) {
+            console.log(error.response.status);
+            console.log(error.response.statusText);
+            console.log(error.response.data);
+            res.status(error.response.status).json({
+                error : error.response.data
+            });
+        }
+        //Guardar datos en BD
+        let dmn = {
+            Usr_ID,
+            Dom_Name : domGD.data.domain,
+            Dom_Status : domGD.data.status,
+            Dom_Expires : domGD.data.expires,
+            Dom_RenewDeadline : domGD.data.renewDeadline,
+            Dom_TransferAwayEligibleAt : domGD.data.transferAwayEligibleAt,
+        }
+        try {
+            const dominio = new Dominio(dmn);
+            await dominio.save();
+            //Regresar datos al usuario
+            res.status(200).json({
+                dominio
+            });
+        } catch (error) {
+            //Regresar datos de error al usuario
+            res.status(500).json({
+                error : {
+                    msg : 'Error del sistema, intente de nuevo más tarde o comuníquese con un asesor'
+                }
+            });
+        }
+    }
+    //Guardar datos en BD en caso de ser necesario
+    try {
+        
+        //Regresar datos de error al usuario
+    } catch (error) {
+        
+    }
 }
 
 let modificarDominio = async (req, res, next) => {
